e01266a62d97485ff292321f17cbb0f8bf6af537
venam <patrick@unixhub.net>
2014-09-28 07:03:03 +0300

    comments in a pythonic manner

--- backend/JsonStateHandler.py
+++ backend/JsonStateHandler.py
@@ -5,13 +5,12 @@ The Role of this file is to save the state to json or read the state from json
 the state is a State object (State.py)
 """
 
-"""
-save :: String -> State -> Void
-save the state in json format to a location specified
-"""
-
 
 def save(saveLocation, state):
+    """
+    save :: String -> State -> Void
+    save the state in json format to a location specified
+    """
     # Save the comments and selected to a json file
     saveOutput = "{\n"
     saveOutput += '"comments":\n'
@@ -21,14 +20,13 @@ def save(saveLocation, state):
     saveOutput += "}\n"
     open(saveLocation, 'w').write(saveOutput)
 
-"""
-load :: String -> State (by reference) -> Void
-load the state from a json file into a State object
-WARNING: this function will change the state
-"""
-
 
 def load(loadLocation, state):
+    """
+    load :: String -> State (by reference) -> Void
+    load the state from a json file into a State object
+    WARNING: this function will change the state
+    """
     jfile = open(loadLocation, 'r')
     loaded = json.load(jfile)
     jfile.close()
--- backend/State.py
+++ backend/State.py
@@ -7,57 +7,57 @@ happening
 
 
 class State:
-    """
-    Constructor doesn't take any arguments
-    """
     def __init__(self):
+        """
+        Constructor doesn't take any arguments
+        """
         self.selected = []
         self.comments = {}
 
-    """
-    load :: String -> Void
-    a wrapper to load state from a json file
-    """
     def load(self, loadLocation):
+        """
+        load :: String -> Void
+        a wrapper to load state from a json file
+        """
         self.selected = []
         self.comments = {}
         JsonStateHandler.load(loadLocation, self)
 
-    """
-    save :: String -> Void
-    a wrapper to save state to a json file
-    """
     def save(self, savelocation):
+        """
+        save :: String -> Void
+        a wrapper to save state to a json file
+        """
         JsonStateHandler.save(savelocation, self)
 
-    """
-    addComment :: String -> String -> Void
-    takes the name of a section and a comment
-    it set the comment of this section as the one specified (overwrite)
-    """
     def addComment(self, section, comment):
+        """
+        addComment :: String -> String -> Void
+        takes the name of a section and a comment
+        it set the comment of this section as the one specified (overwrite)
+        """
         self.comments[section] = comment
 
-    """
-    unComment :: String -> Void
-    takes the name of a section and remove the comment associated with it
-    """
     def unComment(self, section):
+        """
+        unComment :: String -> Void
+        takes the name of a section and remove the comment associated with it
+        """
         if section in self.comments:
             del(self.comments[section])
 
-    """
-    addSelect :: String -> Void
-    takes the name of a section and set it as riced
-    """
     def addSelect(self, section):
+        """
+        addSelect :: String -> Void
+        takes the name of a section and set it as riced
+        """
         if section not in self.selected:
             self.selected.append(section)
 
-    """
-    unSelect :: String -> Void
-    takes the name of a section and unset it from the riced things
-    """
     def unSelect(self, section):
+        """
+        unSelect :: String -> Void
+        takes the name of a section and unset it from the riced things
+        """
         if section in self.selected:
             self.selected.remove(section)
